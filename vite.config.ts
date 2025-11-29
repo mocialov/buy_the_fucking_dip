f17ff182a8bcb65c95d7df1f7f7007b8325b33c3
Boris Mocialov <iphoneetc@gmail.com>
2025-11-30 00:08:26 +0100

    Fix base path to match repository name

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/buy_the_fucking_wdip/',
+  base: '/buy_the_dip/',
 })
