90b4a1479ed458addac1a8a75942caabef2a3ce6
Boris Mocialov <iphoneetc@gmail.com>
2025-11-30 00:02:04 +0100

    name change

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/buy_the_dip/',
+  base: '/buy_the_fucking_wdip/',
 })
